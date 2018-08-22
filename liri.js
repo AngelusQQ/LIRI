ceff807f7b8a1d44ab740601de6e5c021e536947
William <william.ma9215@gmail.com>
2018-08-22 15:24:17 -0400

    All functionality added

--- liri.js
+++ liri.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
 
 var Spotify = require('node-spotify-api');
-
-var keys = require('./keys')
-
+var keys = require('./keys');
 var spotify = new Spotify(keys.spotify);
-
 var request = require('request');
+var fs = require('fs');
 
 //Search Keyword Formatter
 var keyword = "";
@@ -20,8 +18,18 @@ if(process.argv[3] !== undefined) {
   }
 }
 
-var formatDateKey = [5, 6, "/", 8, 9, "/", 0, 1, 2, 3];
+//MAIN
+process.argv[2] === "concert-this" ? concert(keyword)
+: process.argv[2] === "do-what-it-says" ? whatItSays()
+: process.argv[2] === "spotify-this-song" ? song(keyword)
+: process.argv[2] === "movie-this" ? movie(keyword)
+: !process.argv[3] ? console.log("\nMissing Search Parameter! \nUSAGE: node liri.js <search type> <keyword>")
+: console.log("\nUSAGE: node liri.js <search type> <keyword>");
 
+//-------------------------------------------[functions]--------------------------------------------------------------
+
+//format date for concert dates
+var formatDateKey = [5, 6, "/", 8, 9, "/", 0, 1, 2, 3];
 function formatDate(input) {
   var date = ""
   for(var i = 0; i < formatDateKey.length; i++) {
@@ -30,48 +38,8 @@ function formatDate(input) {
   return date;
 }
 
-//Main
-if(process.argv[2] === "help") { console.log("user inputed help") }
-
-else if(process.argv[2] === "concert-this" && process.argv[3] !== undefined) {
-  request("https://rest.bandsintown.com/artists/" + keyword + "/events?app_id=codingbootcamp", function (error, response, body) {
-    var temp = JSON.parse(body);
-    console.log("\r\n------------------------------------------------------------");
-    for(var i = 0; i < temp.length; i++) {
-      console.log("\r\nVenue Name:", temp[i].venue.name);
-      console.log("\r\nLocation:", temp[i].venue.city + ", " + temp[i].venue.region);
-      console.log("\r\nDate:", formatDate(temp[i].datetime));
-      console.log("\r\n------------------------------------------------------------");
-    }
-  });
-}
-
-else if(process.argv[2] === "spotify-this-song") {
-  if(!process.argv[3]) { keyword = "The Sign" }
-  spotify.search({type: 'track', query: keyword}, function (err, data) {
-    console.log("\r\n------------------------------------------------------------");
-    for(var i = 0; i < data.tracks.items.length; i++) {
-      console.log("\r\nArtist Name:", data.tracks.items[i].album.artists[0].name);
-      console.log("\r\nSong Name:", data.tracks.items[i].name);
-      console.log("\r\nPreview Link:", data.tracks.items[i].album.external_urls.spotify);
-      console.log("\r\nAlbum:", data.tracks.items[i].album.name);
-      console.log("\r\n------------------------------------------------------------");
-    }
-  });
-}
-
-/*
-* Title of the movie.
-* Year the movie came out.
-* IMDB Rating of the movie.
-* Rotten Tomatoes Rating of the movie.
-* Country where the movie was produced.
-* Language of the movie.
-* Plot of the movie.
-* Actors in the movie.
-*/
-
-else if(process.argv[2] === "movie-this") {
+//OMDB API
+function movie(keyword) {
   if(!process.argv[3]) { keyword = 'Mr. Nobody'; }
   request("http://www.omdbapi.com/?apikey=trilogy&t=" + keyword, function (error, response, body) {
     console.log("\r\n------------------------------------------------------------");
@@ -89,8 +57,45 @@ else if(process.argv[2] === "movie-this") {
   });
 }
 
-else if(process.argv[2] === "do-what-it-says") {  }
+//Spotify API
+function song(keyword) {
+  if(!process.argv[3] && keyword === "") { keyword = "The Sign" }
+  spotify.search({type: 'track', query: keyword}, function (err, data) {
+    console.log("\r\n------------------------------------------------------------");
+    for(var i = 0; i < data.tracks.items.length; i++) {
+      console.log("\r\nArtist Name:", data.tracks.items[i].album.artists[0].name);
+      console.log("\r\nSong Name:", data.tracks.items[i].name);
+      console.log("\r\nPreview Link:", data.tracks.items[i].album.external_urls.spotify);
+      console.log("\r\nAlbum:", data.tracks.items[i].album.name);
+      console.log("\r\n------------------------------------------------------------");
+    }
+  });
+}
 
-else if(!process.argv[3]) { console.log("\nMissing Search Parameter! \nUSAGE: node liri.js <search type> <keyword>"); }
+//Bands in Town API
+function concert(keyword) {
+  if(!process.argv[3]) {
+    console.log("\r\nMissing Artist Name!")
+  } else {
+    request("https://rest.bandsintown.com/artists/" + keyword + "/events?app_id=codingbootcamp", function (error, response, body) {
+      var temp = JSON.parse(body);
+      console.log("\r\n------------------------------------------------------------");
+      for(var i = 0; i < temp.length; i++) {
+        console.log("\r\nVenue Name:", temp[i].venue.name);
+        console.log("\r\nLocation:", temp[i].venue.city + ", " + temp[i].venue.region);
+        console.log("\r\nDate:", formatDate(temp[i].datetime));
+        console.log("\r\n------------------------------------------------------------");
+      }
+    });
+  }
+}
 
-else { console.log("\nUSAGE: node liri.js <search type> <keyword>"); }
+function whatItSays() {
+  fs.readFile('random.txt', 'utf-8', (err, data) => {
+    var temp = data.split(',');
+    temp[0].includes('concert') ? concert(temp[1])
+    : temp[0].includes('spotify') ? song(temp[1])
+    : temp[0].includes('movie') ? movie(temp[1])
+    : console.log("")
+  });
+}
